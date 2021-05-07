7977a85a5421c1e1cb6d4182b08a36c9cfd3d08e
HelloWorld174 <83816889+HelloWorld174@users.noreply.github.com>
2021-05-07 15:59:18 +0500

    Create Cards.php
    <h1>Файл класса карточки для сайта PHP.<h1>
    
    <h2>Понятия</h2>
    Пользовательские функции внутри класса называются методами.
    Пользовательские переменные внутри класса называются свойствами.
    Аргументы это свойства и переменные заключенные внутри скобок функции или метода, использующиеся в качестве данных для выполнения этой функции или метода
    
    Класс Cards.php содержит в себе метод cardsMethod() вызывающий карточку в верстке html с получением информации из базы данных SQL.
    Свойство $host содержит в себе наименование сервера с типом данных строка (String).
    Свойство $user содержит в себе логин пользователя базы данных с типом данных (String).
    Свойство $password содержит в себе пароль пользователя базы данных с типом данных (String).
    Свойство $db_name содержит в себе наименование базы данных с типом данных (String).
    Свойство $link содержит в себе PHP функцию для подключения к базе данных с помощью вышеуказанных свойств в качестве аргумента.

---
+++ Cards.php
@@ -0,0 +1,56 @@
+<?php
+class Cards{ //класс карточки
+  function cardsMethod(){//метод получения карточки
+  $host='localhost';//
+  $user='Container+';
+  $password='Freightliner5';
+  $db_name='chocomaster';
+  $link=mysqli_connect($host,$user,$password,$db_name);
+
+  if ($_POST['dark'] == "../css/dark.css" ){
+  $result=mysqli_query($link, "SELECT image, name, description FROM cards WHERE chocolade_type='dark'") or die(mysqli_error($link));
+  for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+  foreach ($data as $key => $value) {
+print '<div class="col-4">';
+print '<div class="choco_card">';
+print '<div class="col-12 imgCard" style="background: url('.$data[$key]["image"].');background-size: cover; background-position:center;"></div>';
+print '<h3>'.$data[$key]["name"].'</h3>';
+print '<p>'.$data[$key]["description"].'</p>';
+print '<a href="#">Заказать</a>';
+print '</div>';
+print '</div>';
+  }
+  }
+
+  elseif ($_POST['milk'] == "../css/milk.css" ){
+  $result=mysqli_query($link, "SELECT image, name, description FROM cards WHERE chocolade_type='milk'") or die(mysqli_error($link));
+  for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+  foreach ($data as $key => $value) {
+print '<div class="col-4">';
+print '<div class="choco_card">';
+print '<div class="col-12 imgCard" style="background: url('.$data[$key]["image"].');background-size: cover; background-position:center;"></div>';
+print '<h3>'.$data[$key]["name"].'</h3>';
+print '<p>'.$data[$key]["description"].'</p>';
+print '<a href="#">Заказать</a>';
+print '</div>';
+print '</div>';
+  }
+  }
+
+  else{
+  $result=mysqli_query($link, "SELECT image, name, description FROM cards WHERE chocolade_type='white'") or die(mysqli_error($link));
+  for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+  foreach ($data as $key => $value) {
+print '<div class="col-4">';
+print '<div class="choco_card">';
+print '<div class="col-12 imgCard" style="background: url('.$data[$key]["image"].');background-size: cover; background-position:center;"></div>';
+print '<h3>'.$data[$key]["name"].'</h3>';
+print '<p>'.$data[$key]["description"].'</p>';
+print '<a href="#">Заказать</a>';
+print '</div>';
+print '</div>';
+  }
+  }
+}
+}
+ ?>
