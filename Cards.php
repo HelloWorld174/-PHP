12f3fbcd397ad6c38e951c5ccff1deff8d9e9215
HelloWorld174 <83816889+HelloWorld174@users.noreply.github.com>
2021-05-07 16:54:12 +0500

    Update Cards.php

--- Cards.php
+++ Cards.php
@@ -1,17 +1,17 @@
 <?php
-class Cards{
-  function cardsMethod(){
-  $host='localhost';//
-  $user='Container+';
-  $password='Freightliner5';
-  $db_name='chocomaster';
-  $link=mysqli_connect($host,$user,$password,$db_name);
+class Cards{//класс карточки
+  function cardsMethod(){//метод вызова карточки
+  $host='localhost';//свойство заключающее в себе наименование сервера БД mysql
+  $user='Container+';//свойство заключающее в себе логин БД mysql
+  $password='Freightliner5';//свойство заключающее в себе пароль БД mysql
+  $db_name='chocomaster';//свойство заключающее в себе наименование БД mysql
+  $link=mysqli_connect($host,$user,$password,$db_name);//свойство заключающее в себе функцию подключения к БД mysql
 
-  if ($_POST['dark'] == "../css/dark.css" ){
-  $result=mysqli_query($link, "SELECT image, name, description FROM cards WHERE chocolade_type='dark'") or die(mysqli_error($link));
-  for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
-  foreach ($data as $key => $value) {
-print '<div class="col-4">';
+  if ($_POST['dark'] == "../css/dark.css" ){//логическая конструкция if else
+  $result=mysqli_query($link, "SELECT image, name, description FROM cards WHERE chocolade_type='dark'") or die(mysqli_error($link));//свойство заключающее в себе функцию запроса к БД mysqi mysqli_query, которая содержит в качестве аргументов( свойство $link, ключевое слово SELECT выбирающее данные из столбцов таблицы (наимнования указываются после ключевого слова SELECT), ключевое слово FROM, выбирающее таблицу базы данных(наимнование указываются после ключевого слова FROM), ключевое слово WHERE указывающее условие выбора данных из таблицы (условия указываются после ключевого слова WHERE, с помощью операторов сравнения). 
+  for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);//цикл содержащий в качеисве аргументов (в круглых скобках) декларацию наименования пустого массива, функцию перевода данных sql в массив данных и присваивание этих данных вышеуказанному массиву.
+  foreach ($data as $key => $value) {//специальный цикл для массивов, содержащий в качестве аргумента массив данных полученных из базы данных ($data), разбивающий на пары ключ - значение (as $key => $value), и использующий эти данные для вывода на экран в определенной верстке внутри тела цикла (в фигурных скобках)
+print '<div class="col-4">';//оператор вывода на экран
 print '<div class="choco_card">';
 print '<div class="col-12 imgCard" style="background: url('.$data[$key]["image"].');background-size: cover; background-position:center;"></div>';
 print '<h3>'.$data[$key]["name"].'</h3>';
@@ -22,7 +22,7 @@ print '</div>';
   }
   }
 
-  elseif ($_POST['milk'] == "../css/milk.css" ){
+  elseif ($_POST['milk'] == "../css/milk.css" ){//второе условие логической конструкции if else, указанной выше (смотри комментарий на строке 10)
   $result=mysqli_query($link, "SELECT image, name, description FROM cards WHERE chocolade_type='milk'") or die(mysqli_error($link));
   for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
   foreach ($data as $key => $value) {
@@ -37,7 +37,7 @@ print '</div>';
   }
   }
 
-  else{
+  else{//код выполняющийся в случае невыполнения условий логической конструкции if else, указанной выше (смотри комментарий на строках 10, 25)
   $result=mysqli_query($link, "SELECT image, name, description FROM cards WHERE chocolade_type='white'") or die(mysqli_error($link));
   for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
   foreach ($data as $key => $value) {
